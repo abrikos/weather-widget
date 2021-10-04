4a49c1e80312886b44e4353501b3983ec212b880
abrikos <5422791+abrikos@users.noreply.github.com>
2021-10-04 17:25:54 +0900

    font awesome

--- README.md
+++ README.md
@@ -4,7 +4,6 @@
 https://weather.abrikos.pro
 ### To install the widget, place this code on your page
 ```html
-<meta charset="utf-8">
 <link href="https://weather.abrikos.pro/css/weather-widget.css" rel="stylesheet">
 <div id="weather-widget"></div>
 <script src="https://weather.abrikos.pro/weather-widget.js"></script>
--- src/components/weather-widget/WeatherWidget.vue
+++ src/components/weather-widget/WeatherWidget.vue
@@ -2,9 +2,11 @@
   <div class="weather-widget">
     <div class="widget-header">
       <span>{{ optionsEnabled ? `Settings` : `Weather` }}</span>
-      <span class="gear pointer" @click.prevent="toggleOptions()">{{ optionsEnabled ? `✕` : `⚙` }}</span>
+      <span class="gear pointer" @click.prevent="toggleOptions()">
+        <font-awesome-icon :icon="faCog" v-show="!optionsEnabled"/>
+        <font-awesome-icon :icon="faTimes" v-show="optionsEnabled"/>
+      </span>
     </div>
-    <hr/>
     <div class="weather-content">
       <Settings v-show="optionsEnabled" :cities="cities"/>
       <WeatherTab v-show="!optionsEnabled" :cities="cities"/>
@@ -18,13 +20,15 @@ import "./weather-widget.sass"
 import WeatherTab from "@/components/weather-widget/WeatherTab";
 import Settings from "./Settings";
 import store from "./store";
-
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome"
+import { faSpinner,  faCog, faTimes} from '@fortawesome/free-solid-svg-icons'
 
 export default {
   name: "WeatherWidget",
-  components: {Settings, WeatherTab},
+  components: {Settings, WeatherTab, FontAwesomeIcon},
   data() {
     return {
+      faSpinner, faCog, faTimes,
       optionsEnabled: false,
       cities: store.cities
     }
